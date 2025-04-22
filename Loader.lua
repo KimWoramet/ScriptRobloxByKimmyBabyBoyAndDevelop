0bac1cd7d123a7c8c3de0a5791126fe78b951c87
KimWoramet <101935471+KimWoramet@users.noreply.github.com>
2025-04-22 14:00:09 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
-
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 72829404259339 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KimWoramet/script/refs/heads/main/Anime%20RangerX.lua"))()
+else
+game.Players.LocalPlayer:Kick("Game Check!")
+end
